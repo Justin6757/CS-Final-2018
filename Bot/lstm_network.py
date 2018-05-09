fd9d2395ed61ba7d8d85cf375a143706bbc3a17f
Andrew <li_andrewy@protonmail.com>
2018-05-08 21:11:11 -0400

    Neural net base

---
+++ Bot/lstm_network.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+import numpy as np
+
+
+def load_data():
+    """
+    Load word vectors from GloVe data set (https://nlp.stanford.edu/projects/glove/)
+    :return: None
+    """
+    os.chdir('../Data')
+    print(os.listdir('.'))
+
+    word_list = np.load(
+        'Word Vectors/GloVe_words.npy').tolist()  # Load as list; used for getting indices to access word vectors
+    word_list = [word.decode('UTF-8') for word in word_list]  # Decode all words in UTF-8 format
+
+    word_vectors = np.load('Word Vectors/GloVe_vectors.npy')  # Load as NumPy array
+
+
+def tokenize(sentence):
+    """
+    Remove non-words and split sentence by word
+    :param sentence:
+    :return: list[String]
+    """
+    special_chars = re.compile(r'[\W\|\D]+')  # Replace all characters except letters and numbers
+    tokens = re.sub(special_chars, ' ', sentence)  # Substitute special characters with spaces
+    return tokens.split()
+
+
+if __name__ == '__main__':
+    load_data()
