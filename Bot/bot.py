bb969793b2dfe9100a365b8dd61d48273aecbdea
Unknown <justineli884@gmail.com>
2018-05-22 14:12:51 -0400

    Pickle

--- Bot/bot.py
+++ Bot/bot.py
@@ -1,14 +1,47 @@
 import asyncio
 import time
 
+import pickle
 import discord
 from discord import Game
 from discord.ext.commands import Bot
 
 
+# class for member info
+class DiscordMember:
+    def __init__(self, uid, time):
+        self.id = uid
+        self.score = 25
+        self.time = time
+
+    def __eq__(self, other):
+        if self.id == other.id:
+            return True
+        return False
+
+    def __str__(self):
+        print("ID: " + id + "\n")
+        print("Score: " + score + "\n")
+        print("Time Since Last Message: " + time + "\n")
+        print("--------------------------------")
+
+
+# loads data from previous session of bot
+try:
+    memberList = pickle.load(open("user.pickle", "rb"))
+except (OSError, IOError) as e:
+    memberList = []
+    pickle.dump(memberList, open("user.pickle", "wb"), protocol=pickle.HIGHEST_PROTOCOL)
+
+# Example load and dump using pickle:
+# a = {'hello': 'world'}
+# pickle.dump(a, open('filename.pickle', 'wb'), protocol=pickle.HIGHEST_PROTOCOL)
+#
+# b = pickle.load(open('filename.pickle', 'rb')
+
 BOT_PREFIX = '!'
 # Get at https://discordapp.com/developers/applications/me
-TOKEN = 'NDI3MTQ3Mjc0NDk4MzQyOTMy.DZgT3g.UwYjlweXBF0b1X03r74lUt-v1ms'
+TOKEN = 'NDQzNDQzNzUyNzI4NjU3OTIx.DdNdWQ.3HElcDFaWvTdVyn18XlrTZGhZpM'
 
 client = Bot(command_prefix=BOT_PREFIX)
 
@@ -22,6 +55,13 @@ async def on_ready():
     await client.change_presence(game=Game(name='positively'))
     print('Logged in as ' + client.user.name)
     servers = list(client.servers)
+    for server in servers:
+        for member in server.members:
+            temp = DiscordMember(member.id, time.time())
+            if temp not in memberList:
+                memberList.append(temp)
+    for member in memberList:
+        print(member)
 
 
 async def list_servers():
@@ -53,57 +93,58 @@ async def on_message(message):
         current_time = time.time()
         old_time = current_time
 
-        # if database.find({'UID': message.author.id}).count() == 0:
-        #     database.insert_one({'UID': message.author.id, 'points': MAX_SCORE, 'last message': time.time()})
-        #
-        # database_match = db.serves.find({'UID': message.author.id})
-        # for user in database_match:
-        #     prev_score = user.get('points')
-        #     old_time = user.get('last message')
-        #
-        # time_points = (current_time - old_time) / 600
-        #
-        # new_score = min(prev_score + time_points, MAX_SCORE) + score_change
-        #
-        # db.serves.update({'UID': message.author.id},
-        #                  {'UID': message.author.id,
-        #                   'points': new_score,
-        #                   'last message': current_time})
-        #
-        # if new_score <= BAN_SCORE:
-        #     try:
-        #         await client.ban(message.server.get_member(message.author.id), delete_message_days=0)
-        #     except discord.errors.Forbidden:
-        #         print('Privilege too low')
-        #     else:
-        #         db.serves.remove({'UID': message.author.id})
-        #
-        # elif new_score <= WARNING_SCORE:
-        #     await client.send_message(message.channel,
-        #                               f'**WARNING, <@{message.author.id}>, your positivity score is very low '
-        #                               f'({"{0:0.1f}".format(new_score)}/{MAX_SCORE})**'
-        #                               f'\nYou will be banned if your score reaches {BAN_SCORE} or below.')
+        temp = DiscordMember(message.author.id, time.time())
+        if temp not in memberList:
+            memberList.append(temp)
+
+        for user in memberList:
+            if user.id == message.author.id:
+                temp = user
+                prev_score = user.score
+                old_time = user.time
+
+        time_points = (current_time - old_time) / 600
+
+        new_score = min(prev_score + time_points, MAX_SCORE) + score_change
+
+        temp.score = new_score
+        temp.time = current_time
+
+        if new_score <= BAN_SCORE:
+            try:
+                await client.ban(message.server.get_member(message.author.id), delete_message_days=0)
+            except discord.errors.Forbidden:
+                print('Privilege too low')
+            else:
+                memberList.remove(temp)
+
+        elif new_score <= WARNING_SCORE:
+            await client.send_message(message.channel,
+                                      f'**WARNING, <@{message.author.id}>, your positivity score is very low '
+                                      f'({"{0:0.1f}".format(new_score)}/{MAX_SCORE})**'
+                                      f'\nYou will be banned if your score reaches {BAN_SCORE} or below.')
 
 
 @client.command(pass_context=True)
 async def score(ctx):
-    # if database.find({'UID': ctx.message.author.id}).count() == 0:
-    #     database.insert_one({'UID': ctx.message.author.id, 'points': MAX_SCORE, 'last message': time.time()})
-    #
-    # current_time = time.time()
-    # old_time = current_time
-    #
-    # database_match = db.serves.find({'UID': ctx.message.author.id})
-    # for user in database_match:
-    #     old_time = user.get('last message')
-    #     prev_score = user.get('points')
-    #
-    # time_points = (current_time - old_time) / 600
-    #
-    # db.serves.update({'UID': ctx.message.author.id},
-    #                  {'UID': ctx.message.author.id,
-    #                   'points': min(prev_score + time_points, MAX_SCORE),
-    #                   'last message': current_time})
+    temp = DiscordMember(ctx.message.author.id, time.time())
+    if temp not in memberList:
+        memberList.append(temp)
+
+    current_time = time.time()
+    old_time = current_time
+
+    database_match = db.serves.find({'UID': ctx.message.author.id})
+    for user in database_match:
+        old_time = user.get('last message')
+        prev_score = user.get('points')
+
+    time_points = (current_time - old_time) / 600
+
+    db.serves.update({'UID': ctx.message.author.id},
+                     {'UID': ctx.message.author.id,
+                      'points': min(prev_score + time_points, MAX_SCORE),
+                      'last message': current_time})
 
     await client.send_message(ctx.message.channel,
                               f'{ctx.message.author}\'s score is '
